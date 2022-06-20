c5c17020b29a0f3b1d083d7edbe7ddf82f5ce396
Tiago Medeiros <85853285+Tiagomedeirosds@users.noreply.github.com>
2022-06-20 17:16:46 -0400

    added scroll reveal effect

--- js/index.js
+++ js/index.js
@@ -11,3 +11,20 @@ navLinks.forEach(link => {
     })
     
 })
+
+// scroll effect
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 2500,
+    delay: 400,
+    //reset:true
+})
+
+sr.reveal(`.intro`)
+sr.reveal(`.my-work`,{delay:600, origin: 'left'})
+sr.reveal(`.email`,{delay:600, origin: 'right'})
+sr.reveal(`.about-me`)
+sr.reveal(`.my-services`,{delay:600, origin: 'bottom'})
+sr.reveal(`.portfolio-item-individual`,{delay:600, origin: 'bottom'})
+
